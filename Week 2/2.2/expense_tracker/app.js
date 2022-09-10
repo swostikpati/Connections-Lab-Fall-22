3c875993e3ed6a966566cc8f3b3968c29dc42854
Swostik Pati <swostikpati@gmail.com>
2022-09-10 23:11:07 +0400

    added code

---
+++ Week 2/2.2/expense_tracker/app.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', () => {
+    console.log("page loaded");
+})
+
+let total_amt = 0;
+let total_inc = 0;
+let total_exp = 0;
+
+// const form = document.getElementById("form");
+form.addEventListener("submit", (e) => {
+    console.log(e);
+
+})
