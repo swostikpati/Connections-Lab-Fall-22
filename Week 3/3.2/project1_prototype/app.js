90dc2025d278f4be7735f256b62ac7fd02b36c98
Swostik Pati <swostikpati@gmail.com>
2022-09-18 14:16:43 +0400

    upload

--- Week 3/3.2/project1_prototype/app.js
+++ Week 3/3.2/project1_prototype/app.js
@@ -1,9 +1,13 @@
 window.addEventListener("load", () => {
     console.log("page loaded")
 })
+
 let searchName;
 const txt = document.querySelector("#search_box");
 const pic = document.querySelector(".pictures");
+const unsplash = document.querySelector("#unsplash");
+const init_input = document.querySelector(".input");
+const input2 = document.querySelector(".input2");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault(); //to stop form submission
@@ -14,11 +18,16 @@ form.addEventListener("submit", (e) => {
         .then(data => {
             let desc = data.results[0].alt_description
             console.log(desc);
-            let imgLink = data.results[0].urls.thumb;
+            let imgLink = data.results[0].urls.small;
             pic.innerHTML += (`<img src="${imgLink}" alt="${desc}">`);
 
         })
         .catch(e => {
             console.log("The error is", e);
         })
+})
+
+unsplash.addEventListener("click", () => {
+    init_input.style.display = "none";
+    input2.style.display = "block";
 })
