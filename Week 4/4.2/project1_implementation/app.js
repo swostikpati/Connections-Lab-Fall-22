24bcd3813d60f402ac7999200810253559a7c367
Swostik Pati <swostikpati@gmail.com>
2022-09-26 22:50:03 +0400

    added picture display in website

--- Week 4/4.2/project1_implementation/app.js
+++ Week 4/4.2/project1_implementation/app.js
@@ -12,6 +12,7 @@ const cover_p = document.querySelector(".cover_p img");
 const io_box = document.querySelector(".io-box");
 const output1 = document.querySelector(".output1");
 const back_bt = document.querySelector("#back_bt");
+const pic_container = document.querySelector(".pic_container");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault(); //to stop form submission
@@ -20,12 +21,18 @@ form.addEventListener("submit", (e) => {
     fetch(`https://api.unsplash.com/search/photos?query=${searchName}&client_id=mZPw-CKK4QAA8-T1FUwHhgWwDuHiaeOBD7d8ToskRXg`)
         .then(res => res.json())
         .then(data => {
-            let desc = data.results[0].alt_description
+            let desc = data.results[0].alt_description;
             console.log(desc);
-            let imgLink = data.results[0].urls.small;
-            pic.innerHTML = (`<img src="${imgLink}" alt="${desc}">`) + pic.innerHTML;
+            // let imgLink = data.results[0].urls.small;
+            // pic.innerHTML = (`<img src="${imgLink}" alt="${desc}">`) + pic.innerHTML;
             input2.style.display = "none";
             output1.style.display = "flex";
+            let imgLink;
+            for (let i = 0; i < 6; i++) {
+                imgLink = data.results[i].urls.small;
+                pic_container.innerHTML += (`<img src="${imgLink}" alt="${desc}" id="img_${i + 1}">`);
+                pic_container.style.background = "black";
+            }
 
         })
         .catch(e => {
@@ -49,8 +56,16 @@ cover_p.addEventListener("click", () => {
 back_bt.addEventListener("click", () => {
     output1.style.display = "none";
     input2.style.display = "flex";
+    pic_container.innerHTML = "";
+    console.log("pressed");
 })
 
+pic_container.addEventListener("click", (e) => {
+    let bt = e.target;
+    console.log(bt.id);
+    let img_n = document.querySelector(`#${bt.id}`);
+    console.log(img_n);
+})
 // function setup() {
 //     const myCanvas = createCanvas(400, 400);
 //     myCanvas.parent("canvas-container");
