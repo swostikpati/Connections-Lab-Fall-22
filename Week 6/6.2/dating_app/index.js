2fd2dcf099ee7a878580e887af96199198934c9e
Swostik Pati <swostikpati@gmail.com>
2022-10-08 17:13:42 +0400

    made initial files

---
+++ Week 6/6.2/dating_app/index.js
@@ -0,0 +1,30 @@
+let express = require("express");
+let app = express();
+const PORT = 3000;
+const userJSON = require("./users.json");
+
+
+app.listen(PORT, () => {
+    console.log("Server running on port ", 3000);
+})
+
+app.use("/", express.static("public/page1/"));
+
+app.get("/match", (req, res) => {
+    console.log(req.query);
+    let k = req.query.id;
+    let g;
+    if (k == "1") {
+        g = "male"
+    }
+    else if (k == "2") {
+        g = "female"
+    }
+    if (userJSON[g]) {
+        res.json(userJSON[k]);
+    }
+    // else {
+    //     res.json({ name: "error" }); //can also add res.error
+    // }
+    // res.json(userJSON);
+})
