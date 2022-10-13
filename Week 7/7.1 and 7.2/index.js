9b62d769f82a2352da4367bda958f6333dad5d66
Swostik Pati <swostikpati@gmail.com>
2022-10-13 11:25:52 +0400

    built the server-client interaction

--- Week 7/7.1 and 7.2/index.js
+++ Week 7/7.1 and 7.2/index.js
@@ -1,7 +1,9 @@
 let express = require("express");
 let app = express();
+app.use(express.json()); //add this to understand that it has to pass the info that it is getting in
 const PORT = 3000;
 
+let messages = [];
 
 app.listen(PORT, () => {
     console.log("Server running on port ", 3000);
@@ -9,3 +11,16 @@ app.listen(PORT, () => {
 
 app.use("/", express.static("public"));
 
+app.post("/message", (req, res) => {
+    // console.log(req);
+    // console.log(req.body);
+    messages.push(req.body);
+    console.log(messages);
+    res.send({ "task": "successful" });
+})
+
+app.get("/messages", (req, res) => {
+    res.json({
+        "msgs": messages
+    })
+})
--- Week 7/7.1 and 7.2/public/app.js
+++ Week 7/7.1 and 7.2/public/app.js
@@ -1,6 +1,56 @@
 window.addEventListener("load", () => {
+    //get messages from server every two seconds
+    setInterval(() => {
+        refreshMsgs();
+    }, 2000);
+
     let chatform = document.querySelector("#chat-form");
     chatform.addEventListener("submit", (e) => {
         e.preventDefault();
+        // console.log("chat sent!");
+        let chatName = document.querySelector("#chat-name").value;
+        let chatMsg = document.querySelector("#chat-msg").value;
+        console.log("chat sent!", chatName, chatMsg);
+
+        let msgObj = {
+            "name": chatName,
+            "msg": chatMsg
+        }
+
+        let msgObjJSON = JSON.stringify(msgObj); //very important as only strings can be sent
+        console.log(msgObjJSON);
+        fetch('/message', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' }, //important to make the server side aware that the sent string is of type JSON
+            body: msgObjJSON
+        })
+            //this is not the greatest method - only done here to show the use of another api
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                refreshMsgs();
+            }) //only required for receiving the confirmation from the server side
+        //should have either used ASYNC AWAIT or sent the latest msg and then append it into the HTML
+
+
     })
 })
+
+function refreshMsgs() {
+    //GET request from the API
+    fetch('/messages')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            //clear out the HTML div that contains all the messages
+            //add all the new messages that we have
+        })
+}
+
+/* what happens on form submission?
+1. get the value of what the user typed - DONE
+2a. Sent message to the server - DONE
+2b. store the message on server side 
+3. display the chat message on screen
+
+*/
